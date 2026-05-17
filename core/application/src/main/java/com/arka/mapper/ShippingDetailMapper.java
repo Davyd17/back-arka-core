87240a1ac54aa9dd9c957f3a2f354f30156d7c9d
David <davidcq55@gmail.com>
2026-05-16 21:53:38 -0500

    refactor(shipping detail): remove unused imports from mapper

--- core/application/src/main/java/com/arka/mapper/ShippingDetailMapper.java
+++ core/application/src/main/java/com/arka/mapper/ShippingDetailMapper.java
@@ -1,14 +1,10 @@
 package com.arka.mapper;
 
-import com.arka.dto.in.CreateShippingDetailIn;
 import com.arka.dto.out.ShippingDetailOut;
 import com.arka.model.ShippingDetail;
-import com.arka.model.information.Address;
-import com.arka.model.order.Order;
-import org.mapstruct.BeanMapping;
+
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.Mappings;
+
 
 @Mapper(uses = OrderMapper.class)
 public interface ShippingDetailMapper {

cbf3d30136be306bc69f5cae52645412ba548dc3
David <davidcq55@gmail.com>
2026-05-16 21:57:13 -0500

    refactor(order): remove unused method toDomain from mapper

--- core/application/src/main/java/com/arka/mapper/OrderMapper.java
+++ core/application/src/main/java/com/arka/mapper/OrderMapper.java
@@ -1,26 +1,13 @@
 package com.arka.mapper;
 
-import com.arka.dto.in.CreateOrderIn;
 import com.arka.dto.out.CreateOrderOut;
 import com.arka.dto.out.OrderSummaryOut;
 import com.arka.dto.out.UpdateOrderOut;
 import com.arka.model.order.Order;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.Mappings;
 
 @Mapper(uses = CompanyMapper.class)
 public interface OrderMapper {
-
-    @Mappings({
-            @Mapping(target = "createdAt", ignore = true),
-            @Mapping(target = "updatedAt", ignore = true),
-            @Mapping(target = "id", ignore = true),
-            @Mapping(target = "company.id", source = "companyId"),
-            @Mapping(target = "status", ignore = true),
-            @Mapping(target = "totalPrice", ignore = true)
-    })
-    Order toDomain(CreateOrderIn in);
 
     CreateOrderOut toDTO(Order domain);
 

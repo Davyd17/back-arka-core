86ee102f54bfac94db19bbf7eaa261cd4e3cfeda
David <davidcq55@gmail.com>
2026-05-26 10:29:31 -0500

    refactor: update product mapping, package packages to sub-domains, and use categoryId

--- infrastructure/entry-points/api-rest/src/main/java/com/arka/mappers/ShoppingCartRestMapper.java
+++ infrastructure/entry-points/api-rest/src/main/java/com/arka/mappers/ShoppingCartRestMapper.java
@@ -1,8 +1,6 @@
 package com.arka.mappers;
 
-import com.arka.dto.in.AddItemShoppingCartIn;
-import com.arka.dto.out.ShoppingCartOut;
-import com.arka.request.AddItemShoppingCartRequest;
+import com.arka.cart.dto.ShoppingCartOut;
 import com.arka.response.save.ShoppingCartResponse;
 import org.mapstruct.Mapper;
 
